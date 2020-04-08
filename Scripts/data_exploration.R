79b99834a3947c1e27c3e5cb8592805e30e1b21b
zmeng <zmeng@idrisi.clarku.edu>
2020-04-07 23:10:17 -0400

    project start up

---
+++ Scripts/data_exploration.R
@@ -0,0 +1,19 @@
+library(aRable)
+
+pod_data_A000680 <- ArableClient(device = "A000680", measure = "daily",
+                         start = "2017-06-01", end = "2019-09-06",
+                         email = "lestes@clarku.edu",
+                         password = "rollinghills88",  # replace this w/ actual p
+                         tenant = "clark")
+
+pod_data_A000671 <- ArableClient(device = "A000671", measure = "daily",
+                                 start = "2017-06-01", end = "2019-09-06",
+                                 email = "lestes@clarku.edu",
+                                 password = "rollinghills88",  # replace this w/ actual p
+                                 tenant = "clark")
+
+pod_data_A000667 <- ArableClient(device = "A000667", measure = "daily",
+                                 start = "2017-06-01", end = "2019-09-06",
+                                 email = "lestes@clarku.edu",
+                                 password = "rollinghills88",  # replace this w/ actual p
+                                 tenant = "clark")
