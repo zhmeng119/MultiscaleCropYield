b29b9ebf517ac2857b13c082924829f58e286ce5
zmeng <zhmeng0119@hotmail.com>
2020-04-21 14:33:38 -0400

    explore linear regression and generate some image results

--- Scripts/data_linearRegression.R
+++ Scripts/data_linearRegression.R
@@ -1,16 +1,19 @@
 # Experiment on Linear regression
-rawz_whole <- zoo(p_dat_680_d)
+library(dplyr)
 
-a <- rawz_whole$NDVI %>% filter(.,time %>% substring(.,1,4)==2018) %>% as.numeric()
-b <- c(1:length(p_dat_680_d$NDVI))
+# rawz_whole <- zoo(p_dat_680_d)
+rawz_whole <- p_dat_680_d
+
+a <- c(rawz_whole %>% filter(.,time %>% substring(.,1,4)==2018) %>% select(.,NDVI)) %>% unlist(use.names=FALSE)
+
+b <- c(1:length(a))
 relation <- lm(a~b)
 temp <- data.frame(x = 90)
 result <- predict(relation,temp)
 
 
 # y <- p_dat_680_d$NDVI
-y <- p_dat_680_d %>% filter(.,time %>% substring(.,1,4)==2019)
-y <- y$NDVI
+y <- c(p_dat_680_d %>% filter(.,time %>% substring(.,1,4)==2018) %>% select(.,NDVI) %>% unlist(use.names=FALSE))
 x <- c(1:length(y))
 # x <-  p_dat_680_d %>% filter(.,time %>% substring(.,1,4)==2018) %>% select(SWdw)
 # x <- x$SWdw
@@ -29,3 +32,5 @@ lines(x, predict(fit, data.frame(x=x)), col="red")
 lines(x, predict(fit2, data.frame(x=x)), col="green")
 lines(x, predict(fit3, data.frame(x=x)), col="blue")
 lines(x, predict(fit4, data.frame(x=x)), col="purple")
+legend(0,1, legend = c("first degree","second degree","third degree","fourth degree"),
+       col = c("red","green","blue","purple"), lty=1:2, cex=0.8)
