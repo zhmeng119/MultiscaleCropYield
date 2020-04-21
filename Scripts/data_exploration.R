373503d97a27f0738d1c8b72265a2a5fef75cd9b
zmeng <zhmeng0119@hotmail.com>
2020-04-20 22:07:08 -0400

    finished the construction of APAR

--- Scripts/data_exploration.R
+++ Scripts/data_exploration.R
@@ -61,6 +61,7 @@ p_dat_667_h <- ArableClient(device = "A000667", measure = "hourly",
 library(dplyr)
 library(zoo)
 
+# Part 1: data preprocessing
 
 # A function used to clean up NDVI NA value and replace with interpolated value
 data_preprocess <- function(dataset){
@@ -109,7 +110,69 @@ testdataset <- data_preprocess(testdataset)
 which(is.na(testdataset$NDVI))
 
 
+# Part 2: calculate APRA
 
+# create testing dataset
+Pod_680_d <- p_dat_680_d
+Pod_680_d <- data_preprocess(Pod_680_d)
+# assuming PAR is 0.5 of SWdw
+Pod_680_d <- Pod_680_d %>% select(.,time,NDVI,SWdw) %>% mutate(PAR = SWdw*0.5)
+
+
+# A function used to return the max and min value of NDVI from
+# index 1: min
+# index 2; max
+getMaxMinNDVI <- function(dataset){
+  # find min non-zero value in NDVI
+  minNDVI <- min(dataset$NDVI[dataset$NDVI>0])
+  maxNDVI <- max(dataset$NDVI)
+
+  return(c(minNDVI,maxNDVI))
+}
+# testing function
+Pod_680_d_2018 <- Pod_680_d %>% filter(.,time %>% substring(.,1,4)==2018)
+Pod_680_d_2019 <- Pod_680_d %>% filter(.,time %>% substring(.,1,4)==2019)
+mmNDVI_2018 <- getMaxMinNDVI(Pod_680_d_2018)
+mmNDVI_2019 <- getMaxMinNDVI(Pod_680_d_2019)
+
+
+# calculate Σ(PAR*fPAR) and return a value
+sumPARfPAR <- function(dataset){
+  temp_sum <- 0
+  for(day in 1:length(dataset$time)){
+    # print(day)
+    PAR <- dataset$PAR[day]
+    # calculate fPAR
+    mmNDVI <- getMaxMinNDVI(dataset)
+    fPAR <- 1-((mmNDVI[2]-dataset$NDVI[day])/(mmNDVI[2]-mmNDVI[1]))^0.625
+    temp_sum <- temp_sum + PAR*fPAR
+    # print(temp_sum)
+  }
+  return(temp_sum)
+}
+# testing
+sig_Pod680_2018 <- sumPARfPAR(Pod_680_d_2018)
+sig_Pod680_2019 <- sumPARfPAR(Pod_680_d_2019)
+
+
+# calculate APRA and return a vector that contains APRA for 2018 & 2019
+# index 1: 2018 APAR
+# index 2; 2019 APAR
+APAR <- function(dataset){
+  
+  dataset_2018 <- dataset %>% filter(.,time %>% substring(.,1,4)==2018)
+  dataset_2019 <- dataset %>% filter(.,time %>% substring(.,1,4)==2019)
+  
+  sum_2018 <- sumPARfPAR(dataset_2018)
+  sum_2019 <- sumPARfPAR(dataset_2019)
+  
+  APAR_2018 <- sum_2018*length(dataset_2018$time)
+  APAR_2019 <- sum_2019*length(dataset_2019$time)
+  
+  return(c(APAR_2018,APAR_2019))
+}
+# testing
+APAR_Pod680 <- APAR(Pod_680_d)
 
 
 
